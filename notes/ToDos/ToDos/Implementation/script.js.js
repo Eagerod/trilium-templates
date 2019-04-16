537b0822a8211c511b3e4839ebe4a82acd1f35ca
Aleem Haji <hajial@gmail.com>
2019-04-15 21:19:13 -0400

    Updated todo template so it'll be easier to make fancier.

--- notes/ToDos/ToDos/Implementation/script.js.js
+++ notes/ToDos/ToDos/Implementation/script.js.js
@@ -1,19 +1,46 @@
-const sourceNote = await api.runOnServer(async (srcNoteId) => {
-    const note = await api.getNote(srcNoteId);
-    const sourceNoteAttribute = await note.getAttributeValue("relation", "sourceNote");
-    return await api.getNote(sourceNoteAttribute);
-}, [api.currentNote.noteId]);
+class ToDo {
+    constructor(todoString) {
+        this.ignore = false;
 
-const toDos = sourceNote.content.trim().split("\n");
+        const checkbox = todoString.substring(0, 5);
+        if (checkbox === "- [ ]") {
+            this.isToDo = true;
+        } else if (checkbox === "- [x]") {
+            this.isToDo = false;
+        } else {
+            this.ignore = true;
+        }
 
-$.each(toDos, (i, v) => {
-    if (v.substring(0, 5) === "- [ ]") {
-        $("#todos").append("<input type='checkbox'>" + v.substring(5, v.length) + "<br/>");
+        this.toDoText = todoString.substring(5, todoString.length);
     }
-    else if (v.substring(0, 5) === "- [x]") {
-        $("#dones").append("<input type='checkbox' checked>" + v.substring(5, v.length) + "<br/>");
-    }
-});
 
-const sourceNoteLink = await api.createNoteLink(sourceNote.noteId);
-$("#source-link").append(sourceNoteLink);
+    html() {
+        if (this.ignore) {
+            return "";
+        }
+        return `<input type="checkbox" ${this.isToDo ? "" : "checked"}>${this.toDoText}<br/>`;
+    }
+}
+
+const $todos = $("#todos");
+const $dones = $("#dones");
+const $sourceLink = $("#source-link");
+
+(async () => {
+    const sourceNote = await api.runOnServer(async (srcNoteId) => {
+        const note = await api.getNote(srcNoteId);
+        const sourceNoteAttribute = await note.getAttributeValue("relation", "sourceNote");
+        return await api.getNote(sourceNoteAttribute);
+    }, [api.currentNote.noteId]);
+
+    const toDos = sourceNote.content.trim().split("\n");
+
+    $.each(toDos, (i, v) => {
+        const todo = new ToDo(v);
+        const $list = todo.isToDo ? $todos : $dones;
+        $list.append(todo.html());
+    });
+
+    const sourceNoteLink = await api.createNoteLink(sourceNote.noteId);
+    $sourceLink.append(sourceNoteLink);
+})();
